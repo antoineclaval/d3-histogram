56a21ac8af1f7d4f7965c4f6ef853869451dea34
antoineclaval <antoine.claval@gmail.com>
2015-11-14 14:28:25 -0600

    make chart responsive

--- histogram.js
+++ histogram.js
@@ -368,10 +368,15 @@ function histogramChart(dataset, myOptions)
 	//=========================================================================================================================
 	//create the svg where we are going to plot the histogram
 	//add svg to DOM
-	var mySvg = d3.select("#container").append("svg")
+	var mySvg = d3.select("div#container")
+   		.classed("svg-container", true) //container class to make it responsive
+		.append("svg")
+		.attr("preserveAspectRatio", "xMinYMin meet")
+		.attr("viewBox", "0 0 400 300")
+		.classed("svg-content-responsive", true)
 		.attr("id", "chart") //add style to svg, defined in main.css													
-		.attr("width", width + margin.left + margin.right) //The svg does not have margin
-		.attr("height", height + margin.top + margin.bottom) //The svg does not have margin
+		//.attr("width", width + margin.left + margin.right) //The svg does not have margin
+		//.attr("height", height + margin.top + margin.bottom) //The svg does not have margin
 		//Responsive svg
 		//.call(responsivefy)
 		.append("g") //not sure what this group is doing		
